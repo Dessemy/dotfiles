8aec558073e81cc758e912a76ac0c719ef09262d
Dessemy <Dessmy@proton.me>
2026-06-06 09:01:10 +0700

    Update looknfeel.lua

--- .config/hypr/modules/looknfeel.lua
+++ .config/hypr/modules/looknfeel.lua
@@ -16,8 +16,8 @@ hl.config({
     },
 
     decoration = {
-        rounding       = 10,
-        rounding_power = 10,
+        rounding       = 0,
+        rounding_power = 0,
 
         active_opacity   = 1.0,
         inactive_opacity = 0.8,
