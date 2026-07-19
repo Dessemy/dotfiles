a1af912d4e0fa8a79063fd7ec1279669b88eb424
Dessemy <Dessmy@proton.me>
2026-07-19 15:55:11 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -37,12 +37,12 @@ fi
 log "Installing desktop and CLI packages via yay"
 yay -S --needed --noconfirm \
     ly seatd hyprland xdg-desktop-portal-hyprland qt5-wayland qt6-wayland smartmontools grim slurp nodejs npm openssh wget \
-    brightnessctl playerctl reflector libnotify libqalculate switcheroo-control wl-clipboard wf-recorder cliphist \
+    brightnessctl playerctl reflector libnotify libqalculate switcheroo-control wl-clipboard cliphist \
     foot herdr-bin neovim yazi bluetui impala wiremix btop cava ttyper mpv rmpc mpd 7zip zip unzip \
     zsh starship zoxide jq fd fzf fastfetch eza bat ripgrep imagemagick ffmpeg \
     ttf-firacode-nerd noto-fonts noto-fonts-cjk noto-fonts-emoji \
     hyprlock hyprsunset hyprpicker hyprpaper hyprpolkitagent \
-    qutebrowser vesktop-bin steam lutris \
+    qutebrowser obs-studio vesktop-bin steam lutris \
     fuzzel waybar mako nwg-look
 
 log "Removing pre-existing nvidia-open (conflicts with nvidia-open-dkms)"
